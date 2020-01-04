a0f009bc5952a155399da7eabab211b78766b510
Kaushik Kasibhatla <kaushik.kasibhatla@gmail.com>
2020-01-04 21:40:32 +0530

    GRE_express server commit

---
+++ graphql-poc/src/server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
+var mysql = require("mysql");
+
+var schema = buildSchema(`type Query { hello: String }`);
+var root = { hello: () => "World" };
+
+var app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({ schema: schema, rootValue: root, graphiql: true })
+);
+
+app.use((req, res, next) => {
+  req.mysqlDb = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Password1",
+    database: "userapp"
+  });
+  req.mysqlDb.connect();
+  next();
+});
+
+app.listen(4000);
+
+console.log("Running a GraphQL API server at localhost:4000/graphql");
